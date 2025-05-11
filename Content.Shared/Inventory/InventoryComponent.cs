6a70a7686849cd6e22aec2f42d79e7924f687847
fenndragon <fenndragon@gmail.com>
2025-05-11 15:40:07 -0600

    Revert "displacement edit"
    This reverts commit f127d9a5c7d07e1e69ca75093db47fa5ee7ae982.

--- Content.Shared/Inventory/InventoryComponent.cs
+++ Content.Shared/Inventory/InventoryComponent.cs
@@ -1,5 +1,4 @@
-using Content.Shared.DisplacementMap;
-using Content.Shared.DisplacementMap;
+﻿using Content.Shared.DisplacementMap;
 using Robust.Shared.Containers;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
@@ -18,20 +17,6 @@ public sealed partial class InventoryComponent : Component
     public SlotDefinition[] Slots = Array.Empty<SlotDefinition>();
     public ContainerSlot[] Containers = Array.Empty<ContainerSlot>();
 
-    [DataField]
-    public Dictionary<string, DisplacementData> Displacements = new();
-
-    /// <summary>
-    /// Alternate displacement maps, which if available, will be selected for the player of the appropriate gender.
-    /// </summary>
-    [DataField]
-    public Dictionary<string, DisplacementData> FemaleDisplacements = new();
-
-    /// <summary>
-    /// Alternate displacement maps, which if available, will be selected for the player of the appropriate gender.
-    /// </summary>
-    [DataField]
-    public Dictionary<string, DisplacementData> MaleDisplacements = new();
     [DataField]
     public Dictionary<string, DisplacementData> Displacements = new();
 
